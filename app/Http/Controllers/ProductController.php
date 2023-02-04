0d92f2be418904fb762cd95b7a025ddcb5114dea
Dicky Khusnaedy <dickykhusnaedy98@gmail.com>
2023-02-04 14:01:17 +0700

    update CRUD Product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -21,9 +21,21 @@ class ProductController extends Controller
 		if (request()->ajax()) {
 			$query = Product::query();
 			return DataTables::of($query)
-				->editColumn('price', function ($item) {
-					return number_format($item->price, 2, '.', ',');
+				->addColumn('action', function ($item) {
+					return '
+						<a href="' . route('dashboard.product.edit', $item->id) . '" class="bg-gray-500 text-white rounded-md font-bold text-xs px-2 py-1 m-2">Edit</a>
+						<form class="inline-block" action="' . route('dashboard.product.destroy', $item->id) . '" method="POST">
+						<button class="bg-red-500 text-white rounded-md font-bold text-xs px-2 py-1 m-2">
+							Hapus
+						</button>
+						' . method_field('delete') . csrf_field() . '
+						</form>
+					';
 				})
+				->editColumn('price', function ($item) {
+					return 'IDR ' . number_format($item->price, 2, '.', ',');
+				})
+				->rawColumns(['action'])
 				->make();
 		}
 		return view('pages.dashboard.product.index');
@@ -72,9 +84,9 @@ class ProductController extends Controller
 	 * @param  int  $id
 	 * @return \Illuminate\Http\Response
 	 */
-	public function edit($id)
+	public function edit(Product $product)
 	{
-		//
+		return view('pages.dashboard.product.edit', compact('product'));
 	}
 
 	/**
@@ -84,9 +96,14 @@ class ProductController extends Controller
 	 * @param  int  $id
 	 * @return \Illuminate\Http\Response
 	 */
-	public function update(Request $request, $id)
+	public function update(ProductRequest $request, Product $product)
 	{
-		//
+		$data = $request->all();
+		$data['slug'] = Str::slug($request->name);
+
+		$product->update($data);
+
+		return redirect()->route('dashboard.product.index');
 	}
 
 	/**
@@ -95,8 +112,9 @@ class ProductController extends Controller
 	 * @param  int  $id
 	 * @return \Illuminate\Http\Response
 	 */
-	public function destroy($id)
+	public function destroy(Product $product)
 	{
-		//
+		$product->delete();
+		return redirect()->route('dashboard.product.index');
 	}
 }
---
+++ resources/views/pages/dashboard/product/edit.blade.php
@@ -0,0 +1,77 @@
+<x-app-layout>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+      Product &raquo; {{ $product->name }} &raquo; Edit
+    </h2>
+  </x-slot>
+
+  <x-slot name="style">
+    <style>
+      .ck-editor__editable[role="textbox"] {
+        /* editing area */
+        min-height: 200px;
+      }
+    </style>
+  </x-slot>
+
+  <div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      @if ($errors->any())
+        <div class="mb-5" role="alert">
+          <div class="bg-red-500 text-white font-bold rounded-t px-4 py-2">
+            There's something wrong!
+          </div>
+          <div class="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
+            <ul>
+              @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+              @endforeach
+            </ul>
+          </div>
+        </div>
+      @endif
+      <form action="{{ route('dashboard.product.update', $product->id) }}" method="post" class="w-full"
+        enctype="multipart/form-data">
+        @csrf
+        @method('put')
+        <div class="flex flex-wrap -mx-3 mb-6">
+          <div class="w-full px-3 mb-5">
+            <label class="block uppercase tracking-wide text-grey-700 text-xs font-bold mb-2">Name</label>
+            <input type="text" name="name" id="name" placeholder="Product Name"
+              value="{{ old('name') ?? $product->name }}"
+              class="block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+          </div>
+          <div class="w-full px-3 mb-5">
+            <label class="block uppercase tracking-wide text-grey-700 text-xs font-bold mb-2">Description</label>
+            <textarea name="description" id="description"
+              class="block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">{!! old('description') ?? $product->description !!}</textarea>
+          </div>
+          <div class="w-full px-3 mb-5">
+            <label class="block uppercase tracking-wide text-grey-700 text-xs font-bold mb-2">Price</label>
+            <input type="number" name="price" id="price" placeholder="Product Price"
+              value="{{ old('price') ?? $product->price }}"
+              class="block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+          </div>
+          <div class="w-full flex justify-end px-3 mt-5">
+            <button type="submit"
+              class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 text-sm rounded-full shadow-lg">
+              Update Product
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+
+  <x-slot name="script">
+    <script src="https://cdn.ckeditor.com/ckeditor5/36.0.0/classic/ckeditor.js"></script>
+
+    <script>
+      ClassicEditor
+        .create(document.querySelector('#description'))
+        .catch(error => {
+          console.error(error);
+        });
+    </script>
+  </x-slot>
+</x-app-layout>
